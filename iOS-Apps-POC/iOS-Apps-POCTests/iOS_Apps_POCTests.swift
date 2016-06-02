781a9648cf213263b1c813f84b255eaeb9b5e2fb
Jerry Yuan <jerryjyuan@jwondersconsulting.com>
2016-06-01 19:20:16 -0700

    Add initial draft source code into github for Xcode integration with github

---
+++ iOS-Apps-POC/iOS-Apps-POCTests/iOS_Apps_POCTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_Apps_POCTests.swift
+//  iOS-Apps-POCTests
+//
+//  Created by Jerry Yuan on 6/1/16.
+//  Copyright © 2016 JWonders Consulting, LLC. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Apps_POC
+
+class iOS_Apps_POCTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
